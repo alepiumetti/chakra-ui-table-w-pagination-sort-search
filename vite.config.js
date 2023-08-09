9494ca66c371625e44a082ceb8d46a415de31f7a
Alejandro Piumetti <alepiumetti@hotmail.com>
2023-08-09 14:34:44 -0300

    [ADD] cambio en vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chakra-ui-table-w-pagination-sort-search/",
+  // base: "/chakra-ui-table-w-pagination-sort-search/",
   build: {
     lib: {
       entry: path.resolve("src", "components/PaginationTable.jsx"),
-      name: "table-pagination-chakra-ui",
+      name: "PaginationTable",
       fileName: (format) => `table-pagination-chakra-ui.${format}.js`,
     },
 
